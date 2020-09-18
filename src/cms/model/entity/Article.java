083774a9d675f224de461caf8ba51a3e6eac8b72
mgk <mgk.021@gmail.com>
2020-09-18 12:18:17 +0430

    some changes

--- src/cms/model/entity/Article.java
+++ src/cms/model/entity/Article.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class Article {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Temporal(TemporalType.DATE)
